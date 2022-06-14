2d028406db50f90bd23ae6a06ac92561a9926e24
Harsh Agrawal <harshagrawal007@gmail.com>
2022-06-13 23:25:22 -0700

    238_Product_of_Array_Except_Self.java

---
+++ Arrays/238_Product_of_Array_Except_Self.java
@@ -0,0 +1,21 @@
+public class ProductOfArrayExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Perform Prefix product operation by traversing Left -> Right
+        int prefix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Perform Postfix product operation by traversing Right -> Left
+        int postfix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return result;
+    }
+}
