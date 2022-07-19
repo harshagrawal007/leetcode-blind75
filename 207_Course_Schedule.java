556f7c35ae17f7a4689b82191612c0ea3ba7e249
Harsh Agrawal <harshagrawal007@gmail.com>
2022-07-19 13:44:33 -0700

    Create 207_Course_Schedule.java

---
+++ 207_Course_Schedule.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        // add as many arraylist as the number of courses
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        //get the list for x in pre[x,y] and add y to the list
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        // array equl to size of courses
+        int[] visited = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                if (isCyclic(adj, visited, i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    // 0 not visited
+    // visited
+    // 2 means cyclic, see value 2 again, return iscyclic true
+// if is cyclic true return true isycclic
+    
+    private boolean isCyclic(List<List<Integer>> adj, int[] visited, int curr) {
+        if (visited[curr] == 2) {
+            return true;
+        }
+        
+        visited[curr] = 2;
+        for (int i = 0; i < adj.get(curr).size(); i++) {
+            if (visited[adj.get(curr).get(i)] != 1) {
+                if (isCyclic(adj, visited, adj.get(curr).get(i))) {
+                    return true;
+                }
+            }
+        }
+        visited[curr] = 1;
+        return false;
+    }
+}
