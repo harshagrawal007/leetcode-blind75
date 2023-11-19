847a9bd01a514653a36621ba01ce23d97bfed63d
harshagrawal007 <harshagrawal007@gmail.com>
2023-11-18 20:31:37 -0800

    Modify 57_Insert_Interval.java

--- Intervals/57_Insert_Interval.java
+++ Intervals/57_Insert_Interval.java
@@ -1,3 +1,40 @@
+// faster on leetcode
+// we are navigating only once through the list of interval 
+//time complexity = O(n)
+// space complexity = O(n) we created extra space arraylist which stores the result
+class Solution {
+
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+      List<int[]> res = new ArrayList<>();
+      int newStart = newInterval[0], newEnd = newInterval[1];
+      for (int[] interval : intervals) {
+          // current end is less than new Start
+          // or the new interval is null since its been already added
+          if (newInterval == null || interval[1] < newStart) {
+              res.add(interval); 
+
+          } else if (interval[0] > newEnd) {
+              // current start is greater than new end
+              res.add(newInterval);
+              res.add(interval);
+              // assign new interval as null, 
+              //to mark that new interval is added to the result
+              newInterval = null;
+          } else {
+              newInterval[0] = Math.min(interval[0], newInterval[0]);
+              newInterval[1] = Math.max(interval[1], newInterval[1]);
+          }
+      }
+      // if new interval is not null that means its not added to result 
+      if (newInterval != null) res.add(newInterval);
+      return res.toArray(new int[res.size()][]);
+  }
+}
+
+
+// below solution is slower on leetcode
+// complexity time: 0(n) since we are only visiting each interval once
+// space complexity 0(n) we created extra space arraylist
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         
