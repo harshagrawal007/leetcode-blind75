0850cc945911b9d9d4d37c600d3496b6611b4514
harshagrawal007 <harshagrawal007@gmail.com>
2023-11-18 18:51:15 -0800

    modify 252_Meeting_Rooms

--- Intervals/252_Meeting_Rooms.java
+++ Intervals/252_Meeting_Rooms.java
@@ -1,17 +1,16 @@
 class Solution {
-    public boolean canAttendMeetings(int[][] intervals) {
-        if (intervals == null)
-            return false;
+  public boolean canAttendMeetings(int[][] intervals) {
+    if (intervals == null)
+      return false;
 
-  // Sort the intervals by start time
-  Arrays.sort(intervals, (a,b) -> a[0] - b[0] );
-  
-  for (int i = 1; i < intervals.length; i++) {
+    // Sort the intervals by start time
+    Arrays.sort(intervals, (a,b) -> a[0] - b[0] );
+
+    for (int i = 1; i < intervals.length; i++) {
       if (intervals[i][0] < intervals[i - 1][1]) {
         return false;
+      }
     }
+    return true;
   }
-  return true;
-}
-    
 }
