6238da0bcc00c0075a842652124fb359a3b33bb6
harshagrawal007 <harshagrawal007@gmail.com>
2023-11-22 10:50:54 -0800

    Modify 238_Product_of_Array_Except_Self.java

--- Arrays_and_hashing/238_Product_of_Array_Except_Self.java
+++ Arrays_and_hashing/238_Product_of_Array_Except_Self.java
@@ -19,3 +19,24 @@ public class ProductOfArrayExceptSelf {
         return result;
     }
 }
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        // Perform left product operation by traversing Left -> Right
+        result[0] =1;
+        for (int i = 1; i < n; i++) {
+            result[i] = nums[i-1] * result[i-1];
+        }
+
+        // Perform right product operation by traversing Right -> Left
+        int right_product = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right_product;
+            right_product = right_product * nums[i];
+        }
+        return result;
+        
+    }
+}
